afe50a529a9fe0274a0c1521cc582f5f8419c315
imronreviady <imronreviady2@gmail.com>
2017-12-13 02:06:04 +0700

    implement modal for show freelancer detail

--- public/backend/admin/manage_freelancer.php
+++ public/backend/admin/manage_freelancer.php
@@ -378,7 +378,7 @@ var DatatableDataLocalDemo = function () {
                             </a>\
                             <div class="dropdown-menu dropdown-menu-right">\
                                 <a class="dropdown-item" href="<?=base_url()?>admin/freelancers/change_status/' + row.freelancer_id + '"><i class="la ' + active_or_not[row.is_active].icon + '"></i> ' + active_or_not[row.is_active].title + '</a>\
-                                <a class="dropdown-item" href="#"><i class="la la-eye"></i> Details</a>\
+                                <a href="javascript:;" class="dropdown-item detailbtn" data-page="detail_freelancer" onClick="showFullModal(\'' + row.freelancer_id + '\')"><i class="la la-eye"></i> Details</a>\
                                 <a href="javascript:;" class="dropdown-item deletebtn" data-type="freelancer" onClick="deleteData(\'' + row.freelancer_id + '\')"><i class="la la-trash"></i> Delete</a>\
                             </div>\
                         </div>\
